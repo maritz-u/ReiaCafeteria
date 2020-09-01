4468d272f24ed7d23b3ddf6c01ffc88eba4e208c
CatalinaJL <catalina.p.jl@gmail.com>
2020-09-01 11:22:19 -0400

    División de componentes, y creación de nuevas páginas

--- src/components/second-page.js
+++ src/components/second-page.js
@@ -1,9 +1,11 @@
 import React from 'react'; 
+import Logo from './Logo.js'
 
 
 const Secondpage= () => {
     return (
       <div className="Botonbox">
+        <Logo />
          <input type="text" id="codigomesero" placeholder="Ingresa tu código"></input>
         <button type="submit" id="boton cocina">Entrar</button>
       </div>
