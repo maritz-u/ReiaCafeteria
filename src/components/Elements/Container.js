f8a7ebb2a852b6b42c92a601847263535ecfb0e0
CatalinaJL <catalina.p.jl@gmail.com>
2020-09-10 13:20:29 -0300

    Cocina: ideas to show data in container

--- src/components/Elements/Container.js
+++ src/components/Elements/Container.js
@@ -1,14 +1,13 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import db from '../../firebase.js';
 
 const ContainerCocina = () => {
 
     const [cocina, setCocina] = useState([]);
-
     const getOrder = db.collection("pedido").onSnapshot((querySnapshot) => {
         let docs = [];
         querySnapshot.forEach((doc) => {
-            docs.push({ ...doc.data(), id:doc.id});
+            docs.push({ ...doc.data(), id: doc.id });
         });
         console.log(docs);
         docs.map((data) => {
@@ -23,8 +22,14 @@ const ContainerCocina = () => {
                 "001 Orlando Bloom"
             </h3>
             <div className="checkbox">
-                <input type="checkbox" name="" id="" /> Café
-                <input type="checkbox" name="" id=""/> Pie de Limón 
+                {cocina.map((data) => {
+                    data.map((item) =>{
+                        console.log(item.name);
+                    })
+                    // return <div key={index}>
+                    //     <input type="checkbox" name={data.name} />{data.name}
+                    // </div>
+                })}
             </div>
             <div className="progress">
                 <button type="submit">Entregar</button>
