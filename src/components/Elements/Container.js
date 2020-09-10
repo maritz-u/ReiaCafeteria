579e6c326ca0d4d1b13742d0af584c8307a70642
CatalinaJL <catalina.p.jl@gmail.com>
2020-09-10 12:18:52 -0300

    Cocina: get firebase data from collection pedido, and show in console

--- src/components/Elements/Container.js
+++ src/components/Elements/Container.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+import db from '../../firebase.js';
 
 const ContainerCocina = () => {
+
+    const [cocina, setCocina] = useState([]);
+
+    const getOrder = db.collection("pedido").onSnapshot((querySnapshot) => {
+        let docs = [];
+        querySnapshot.forEach((doc) => {
+            docs.push({ ...doc.data(), id:doc.id});
+        });
+        console.log(docs);
+        docs.map((data) => {
+            cocina.push(data.name);
+        });
+        setCocina(cocina);
+        console.log(cocina);
+    });
     return (
         <div className="kitchen box">
             <h3>
