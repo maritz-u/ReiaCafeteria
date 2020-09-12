9f18a69aa7409938281cb207adbfec62eb9e50a3
CatalinaJL <catalina.p.jl@gmail.com>
2020-09-12 18:52:39 -0300

    Cocina.JS = change of function to get information of firestore. It was change from container.js to cocina.js

--- src/components/Elements/Container.js
+++ src/components/Elements/Container.js
@@ -1,63 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import db from '../../firebase.js';
-
-const cocinaOrder = [];
+import React from 'react';
 
 const ContainerCocina = () => {
 
-    const [cocina, setCocina] = useState([]);
-    const getOrderTable = () => {
-        db.collection('pedido').onSnapshot((querySnapshot) => {
-            querySnapshot.forEach(doc => {
-                // console.log(doc.data());
-                // console.log(doc.id);
-                let id = doc.id;
-                let mesa = doc.data().mesa[2];
-                let productos = doc.data().name;
-                // console.log(id, mesa, productos);
-                cocinaOrder.push({id,mesa,productos});
-                // let mesa = doc.data().mesa[2];
-                // // console.log(mesa);
-                // let productos = doc.data().name;
-                // // console.log(productos);
-                // cocinaOrder.push({ "mesa": mesa, "productos": productos});
-                // setCocina(cocinaOrder);
-                // console.log(cocina);
-            });
-            
-            setCocina(cocinaOrder);
-            // console.log(cocina);
-        });
-    };
-    // getOrder();
-    useEffect(()=>{
-        getOrderTable();
-    }, []);
-    
-    // const pedidoCocina = cocina;
-    // const arrayPedidoCocina= Array.from(pedidoCocina);
-
-    // const mesa= arrayPedidoCocina.filter(function (arrayPedidoCocina) {
-    //         return arrayPedidoCocina.mesa === "mesa";
-    // });
-    // console.log(mesa);
-
     return (
         <div className="kitchen box">
-            <div className="half">
-                {cocinaOrder.map(({ mesa, productos, id }) => {
-                    // console.log(mesa,productos,id);
-                    return <div key={id}>
-                        <h4>{mesa}</h4>
-                        {productos.map((data, i) => {
-                            return <div key={i}>
-                                <p>{data}</p>
-                            </div>
-                        })}
-                    </div>
-                })};
-
-                    <button className="botonEntrega">Entregar</button>
+            <div className="firsthalf">
             </div>
         </div>
 
